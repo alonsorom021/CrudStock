d0a3ee9694d2cec9d55a713cf5deea01d9c94d5e
ernestromero <nem.ernestoromero@coppel.com>
2025-02-07 13:58:30 -0600

    Se actualiza la entidad producto

--- src/main/java/com/control/stock/entity/Producto.java
+++ src/main/java/com/control/stock/entity/Producto.java
@@ -3,36 +3,21 @@ package com.control.stock.entity;
 import jakarta.persistence.*;
 
 import java.math.BigDecimal;
-/**
- * La clase Producto representa un producto en la aplicación.
- * Se mapea a la tabla "producto" en la base de datos.
- */
+
 @Entity
 @Table(name = "producto")
 public class Producto {
 
-    /**
-     * El ID del producto. Este campo se genera automáticamente.
-     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    /**
-     * El nombre del producto.
-     */
+ 
     @Column(name = "nombre")
     private String nombre;
-
-    /**
-     * La cantidad del producto en stock.
-     */
+ 
     @Column(name = "cantidad")
     private Integer cantidad;
 
-    /**
-     * El precio unitario del producto.
-     */
     @Column(name = "precio_unitario")
     private BigDecimal precioUnitario;
 
